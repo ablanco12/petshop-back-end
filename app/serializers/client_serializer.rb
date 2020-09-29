657e3ef30922272eeb47f2c05dafee7c80450ecb
Andrew - Joseph Blanco <44991800+ablanco12@users.noreply.github.com>
2020-09-29 16:30:07 -0500

    fixed syntax

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -1,4 +1,4 @@
-class ClientSerializers < ActiveModel::Serializer
+class ClientSerializer < ActiveModel::Serializer
     include Rails.application.routes.url_helpers
   
     attributes :lastname, :firstname, :homephone, :workphone
